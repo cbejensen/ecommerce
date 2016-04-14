6c1b2bd5c0580443c236321cfcde15cc65a8fdb4
Christian <cbejensen@gmail.com>
2016-04-14 14:07:51 -0600

    replaced mongojs with mongoose

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
-var mongo = require('mongojs');
+var mongoose = require('mongoose');
 
 var app = express();
 
@@ -16,6 +16,13 @@ app.get('/api/products/:id', productsController.findOne);
 app.post('/api/products', productsController.save);
 app.put('/api/products/:id', productsController.update);
 app.delete('/api/products/:id', productsController.remove);
+
+mongoose.connect('mongodb://localhost/products');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to Mongoose')
+});
 
 var port = 8000;
 app.listen(port, function() {
